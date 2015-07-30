312149d1784abe9a85fc4a3f115d66c714eabec5
Ricardo Brandão <ricardo.brandao@outlook.pt>
2015-07-31 00:22:39 +0100

    Add .jsbeautifyrc to automatically format code.

--- app/src/index-viewmodel.js
+++ app/src/index-viewmodel.js
@@ -1,19 +1,27 @@
-'use strict'
+'use strict';
 
 import ko from 'knockout';
 
 import componentHtmlTemplate from 'index-template.html!text';
 
 class IndexViewModel {
-  constructor(params, componentInfo) {
-    this.variable = ko.observable('Hello, World!');
-    
-    params.isInitialized(true);
-  }
+	constructor(params, componentInfo) {
+		this.variable = ko.observable('Hello, World!');
+
+		params.isInitialized(true);
+
+		if (true) {
+			return false;
+		}
+
+		this.myArrowFn = () => {
+			console.log('This is just a arrow function...');
+		};
+	}
 }
 
 export var viewModel = {
-	createViewModel: function (params, componentInfo) {
+	createViewModel: function(params, componentInfo) {
 		return new IndexViewModel(params, componentInfo);
 	}
 };
