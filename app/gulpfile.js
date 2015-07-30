015b7fbe78cef4d6dbb128e5de8cf072a347b254
Ricardo Brandão <ricardo.brandao@outlook.pt>
2015-07-31 00:48:32 +0100

    Now using gulp-eslint instead of gulp-jshint for the linting task.

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -3,8 +3,7 @@ var runSequence = require('run-sequence');
 var del = require('del');
 var vinylPaths = require('vinyl-paths');
 var babel = require('gulp-babel');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
+var eslint = require('gulp-eslint');
 var changelog = require('conventional-changelog');
 var assign = Object.assign || require('object.assign');
 var fs = require('fs');
@@ -16,23 +15,21 @@ var plumber = require('gulp-plumber');
 var shell = require('gulp-shell');
 
 var path = {
-  source:'src/**/*.js',
-  html:'src/**/*.html',
-  style:'styles/**/*.css',
-  output:'dist/',
-  doc:'./doc',
-  root:'../'
+	source: 'src/**/*.js',
+	html: 'src/**/*.html',
+	style: 'styles/**/*.css',
+	output: 'dist/',
+	doc: './doc',
+	root: '../'
 };
 
-var bundles = [
-    {
-        module: 'index-viewmodel',
-        name: 'bundled'
-    }
-];
+var bundles = [{
+	module: 'index-viewmodel',
+	name: 'bundled'
+}];
 
 var compilerOptions = {
- modules: 'system',
+	modules: 'system',
 	moduleIds: false,
 	comments: false,
 	compact: false,
@@ -40,96 +37,102 @@ var compilerOptions = {
 	optional: []
 };
 
-var jshintConfig = {esnext:true};
-
 gulp.task('clean', function() {
 	return gulp.src([path.output]).pipe(vinylPaths(del));
 });
 
-gulp.task('build-system', function () {
-  return gulp.src(path.source)
-    .pipe(plumber())
-    .pipe(changed(path.output, {extension: '.js'}))
-    .pipe(babel(assign({}, compilerOptions, {modules:'system'})))
-    .pipe(gulp.dest(path.output));
+gulp.task('build-system', function() {
+	return gulp.src(path.source)
+		.pipe(plumber())
+		.pipe(changed(path.output, {
+			extension: '.js'
+		}))
+		.pipe(babel(assign({}, compilerOptions, {
+			modules: 'system'
+		})))
+		.pipe(gulp.dest(path.output));
 });
 
 gulp.task('build-bundles',
 	shell.task(
-		bundles.map(function (bundle) {
+		bundles.map(function(bundle) {
 			return 'jspm bundle ' + bundle.module + ' ' + path.output + bundle.name + '.js --inject';
 		}))
 )
 
-gulp.task('build-html', function () {
-  return gulp.src(path.html)
-    .pipe(changed(path.output, {extension: '.html'}))
-    .pipe(gulp.dest(path.output));
+gulp.task('build-html', function() {
+	return gulp.src(path.html)
+		.pipe(changed(path.output, {
+			extension: '.html'
+		}))
+		.pipe(gulp.dest(path.output));
 });
 
 gulp.task('lint', function() {
-  return gulp.src(path.source)
-    .pipe(jshint(jshintConfig))
-    .pipe(jshint.reporter(stylish));
+	return gulp.src(path.source)
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
 });
 
-gulp.task('bump-version', function(){
-  return gulp.src(['./package.json'])
-    .pipe(bump({type:'patch'})) //major|minor|patch|prerelease
-    .pipe(gulp.dest('./'));
+gulp.task('bump-version', function() {
+	return gulp.src(['./package.json'])
+		.pipe(bump({
+			type: 'patch'
+		})) //major|minor|patch|prerelease
+		.pipe(gulp.dest('./'));
 });
 
 gulp.task('changelog', function(callback) {
-  var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+	var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-  return changelog({
-    repository: pkg.repository.url,
-    version: pkg.version,
-    file: path.doc + '/CHANGELOG.md'
-  }, function(err, log) {
-    fs.writeFileSync(path.doc + '/CHANGELOG.md', log);
-  });
+	return changelog({
+		repository: pkg.repository.url,
+		version: pkg.version,
+		file: path.doc + '/CHANGELOG.md'
+	}, function(err, log) {
+		fs.writeFileSync(path.doc + '/CHANGELOG.md', log);
+	});
 });
 
 gulp.task('build', function(callback) {
-  return runSequence(
-    'clean',
-    ['build-system', 'build-html'],
-	'build-bundles',
-    callback
-  );
+	return runSequence(
+		'clean', ['build-system', 'build-html'],
+		'build-bundles',
+		callback
+	);
 });
 
 gulp.task('serve', ['build'], function(done) {
-  browserSync({
-    open: false,
-    port: 8080,
-    server: {
-      baseDir: [path.root],
-      middleware: function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
-  }, done);
+	browserSync({
+		open: false,
+		port: 8080,
+		server: {
+			baseDir: [path.root],
+			middleware: function(req, res, next) {
+				res.setHeader('Access-Control-Allow-Origin', '*');
+				next();
+			}
+		}
+	}, done);
 });
 
-function reportChange(event){
-  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+function reportChange(event) {
+	console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
 
 gulp.task('watch', ['serve'], function() {
-  gulp.watch(path.source, ['build-system', browserSync.reload]).on('change', reportChange);
-  gulp.watch(path.html, ['build-html', browserSync.reload]).on('change', reportChange);
-  gulp.watch(path.style, browserSync.reload).on('change', reportChange);
+	gulp.watch(path.source, ['build-system', browserSync.reload]).on('change', reportChange);
+	gulp.watch(path.html, ['build-html', browserSync.reload]).on('change', reportChange);
+	gulp.watch(path.style, browserSync.reload).on('change', reportChange);
 });
 
-gulp.task('prepare-release', function(callback){
-  return runSequence(
-    'build',
-    'lint',
-    'bump-version',
-    'changelog',
-    callback
-  );
+gulp.task('prepare-release', function(callback) {
+	return runSequence(
+		'build',
+		'lint',
+		'bump-version',
+		'changelog',
+		callback
+	);
 });
--- app/src/index-bootstrapper.js
+++ app/src/index-bootstrapper.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-import ko from 'knockout'
+import ko from 'knockout';
 
-let vm = {
+const vm = {
   isInitialized: ko.observable(false)
 };
 
 ko.components.register('index', {
   require: 'index-viewmodel',
 	synchronous: true
-})
+});
 
 ko.applyBindings(vm, document.getElementById('js-index-container'));
